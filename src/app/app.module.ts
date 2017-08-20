97d6b275c7587e61cf1fb495af606ab67b8cb24c
johannabzg <johanna.bouzige@gmail.com>
2017-08-20 03:17:40 +0200

    navigation ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,17 @@ import { PresentationComponent } from './presentation/presentation.component';
 import { MonXPComponent } from './mon-xp/mon-xp.component';
 import { MesCompetencesComponent } from './mes-competences/mes-competences.component';
 import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const appRoutes: Routes = [
+  { path: 'presentation', component: PresentationComponent },
+  { path: 'experiences', component: MonXPComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'competence', component: MesCompetencesComponent }
+     
+];
 
 @NgModule({
   declarations: [
@@ -20,9 +31,12 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)  
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ src/app/fondAnimated.ts
@@ -0,0 +1,14 @@
+let granimInstance = {
+    element: '#canvas-background',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+      'default-state': {
+          gradients: [
+              ['#e294dc', '#ffffff'],
+              ['#ffe255', '#29caac']
+          ]
+    }
+}
+    };
