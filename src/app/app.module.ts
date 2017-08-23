e8dffd56aa7b0d8c2099ac37031c8d1e8574f155
johannabzg <johanna.bouzige@gmail.com>
2017-08-23 20:11:05 +0200

    base navigation OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,34 +8,17 @@ import { PresentationComponent } from './presentation/presentation.component';
 import { MonXPComponent } from './mon-xp/mon-xp.component';
 import { MesCompetencesComponent } from './mes-competences/mes-competences.component';
 import { ContactComponent } from './contact/contact.component';
-import { RouterModule, Routes } from '@angular/router';
+import { MentionsComponent } from './mentions/mentions.component';
 import { FormsModule }   from '@angular/forms';
 import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
 import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
-import { MentionsComponent } from './mentions/mentions.component';
-/*
-import 'web-animations-js/web-animations.min';
-import 'hammerjs/hammer';
 
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-
-import 'web-animations-js';
-*/
+/*animation*/
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 const appRoutes: Routes = [
+    { path: '', redirectTo: '/presentation', pathMatch: 'full'},
   { path: 'presentation', component: PresentationComponent  },
   { path: 'experiences', component: MonXPComponent },
   { path: 'contact', component: ContactComponent },
@@ -61,7 +44,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,  
     RouterModule.forRoot(appRoutes),
-    Angular2ImageGalleryModule  
+    Angular2ImageGalleryModule,
+    BrowserAnimationsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,9 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
-  styleUrls: ['./presentation.component.css']
+  styleUrls: ['./presentation.component.css'],
+    animations: [
+  trigger('app-presentation', [
+   state('in', style({transform: 'translateX(0)'})),
+   
+  transition('* => void', [ animate(100, style({transform: 'translateX(100%)'}))
+   
+  ]),
+  transition('* => void', [
+      animate(100, style({transform: 'translateX(100%)'}))
+    ])
+  ])
+]
 })
 export class PresentationComponent implements OnInit {
 
